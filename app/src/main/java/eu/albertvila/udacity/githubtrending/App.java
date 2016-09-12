d6f88d48281fa0c10eaec7243a2a97c7618fa872
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-09-12 18:42:39 +0200

    Add Timber for logging

--- app/src/main/java/eu/albertvila/udacity/githubtrending/App.java
+++ app/src/main/java/eu/albertvila/udacity/githubtrending/App.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.google.android.gms.ads.MobileAds;
 
+import timber.log.Timber;
+
 /**
  * Created by Albert Vila Calvo on 12/9/16.
  */
@@ -12,6 +14,15 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Logging
+        Timber.plant(new Timber.DebugTree() {
+            @Override
+            protected String createStackElementTag(StackTraceElement element) {
+                // Add the line number
+                return super.createStackElementTag(element) + ":" + element.getLineNumber();
+            }
+        });
 
         // Initialize the Google Mobile Ads SDK
         // https://firebase.google.com/docs/admob/android/quick-start#initialize_the_google_mobile_ads_sdk
