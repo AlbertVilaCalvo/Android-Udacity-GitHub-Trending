f3eece221390fc91d5ef0cafadf918776421be6d
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-09-12 22:16:49 +0200

    Fetch github.com/trending with OkHttp and RxJava, and parse HTML with JSoup

--- app/src/main/java/eu/albertvila/udacity/githubtrending/MainActivity.java
+++ app/src/main/java/eu/albertvila/udacity/githubtrending/MainActivity.java
@@ -13,6 +13,24 @@ import android.view.View;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DefaultObserver;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -39,6 +57,78 @@ public class MainActivity extends AppCompatActivity {
                 .addTestDevice("D7A3AA3A1DAE36977EE1652523B33CBD")
                 .build();
         adView.loadAd(adRequest);
+
+        getData()
+                .subscribeOn(Schedulers.io())
+                .map(new Function<String, List<String>>() {
+                    @Override
+                    public List<String> apply(String html) throws Exception {
+                        Document document = Jsoup.parse(html);
+                        Elements elements = document.select(".repo-list-name > a");
+                        List<String> urls = new ArrayList<>();
+                        for (int i = 0; i < elements.size(); i++) {
+                            urls.add(elements.get(i).attr("href"));
+                        }
+                        Timber.d("urls %s", urls);
+                        return urls;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DefaultObserver<List<String>>() {
+                    @Override
+                    public void onNext(List<String> value) {
+                        Timber.d("onNext: %s", value);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Timber.i("onComplete");
+                    }
+                });
+
+
+        /*
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://github.com/trending")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Timber.e(e, "onFailure");
+            }
+
+            @Override
+            public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                Timber.d("onResponse response: %s", response);
+                // Timber.d("onResponse: %s", response.body().string());
+                Document document = Jsoup.parse(response.body().string());
+                response.body().close();
+            }
+        });
+        */
+    }
+
+    private Observable<String> getData() {
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url("https://github.com/trending")
+                .build();
+
+        return Observable.fromCallable(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Response response = client.newCall(request).execute();
+                String html = response.body().string();
+                response.body().close();
+                return html;
+            }
+        });
     }
 
     @Override
