f58c7d70c695dbaba0ddf6c38301c527c3a58b9c
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-09-14 19:26:06 +0200

    Add DbContract, DbOpenHelper and stub DbProvider

---
+++ app/src/main/java/eu/albertvila/udacity/githubtrending/data/db/DbContract.java
@@ -0,0 +1,26 @@
+package eu.albertvila.udacity.githubtrending.data.db;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import eu.albertvila.udacity.githubtrending.BuildConfig;
+
+/**
+ * Created by Albert Vila Calvo on 14/9/16.
+ */
+public class DbContract {
+
+    public static final String AUTHORITY = String.format("%s.provider", BuildConfig.APPLICATION_ID);
+
+    public static final Uri AUTHORITY_URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(AUTHORITY)
+            .build();
+
+    public interface Repo extends BaseColumns {
+        String TABLE = "repo";
+        String URL= "url";
+    }
+
+}
---
+++ app/src/main/java/eu/albertvila/udacity/githubtrending/data/db/DbOpenHelper.java
@@ -0,0 +1,36 @@
+package eu.albertvila.udacity.githubtrending.data.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Albert Vila Calvo on 14/9/16.
+ */
+public class DbOpenHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "repo.db";
+
+    private static final String CREATE_REPO_TABLE = ""
+            + "CREATE TABLE " + DbContract.Repo.TABLE + "("
+            + DbContract.Repo._ID + " INTEGER NOT NULL PRIMARY KEY,"
+            + DbContract.Repo.URL + " TEXT NOT NULL,"
+            + ")";
+
+    public DbOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_REPO_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+}
