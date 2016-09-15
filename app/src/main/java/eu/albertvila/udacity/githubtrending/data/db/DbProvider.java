a9006263f4071798dbd3f68a11e61a52123f7b10
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-09-15 15:57:09 +0200

    DbProvider - Implement insert()

--- app/src/main/java/eu/albertvila/udacity/githubtrending/data/db/DbProvider.java
+++ app/src/main/java/eu/albertvila/udacity/githubtrending/data/db/DbProvider.java
@@ -1,22 +1,24 @@
 package eu.albertvila.udacity.githubtrending.data.db;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 
 public class DbProvider extends ContentProvider {
 
     // Maps a URI to a table
-    private static final int CODE_ALL_REPOS = 100;
+    private static final int MATCH_ALL_REPOS = 100;
 
     // Maps a URI to an int
     private static final UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
 
     static {
-        URI_MATCHER.addURI(DbContract.AUTHORITY, DbContract.Repo.PATH, CODE_ALL_REPOS);
+        URI_MATCHER.addURI(DbContract.AUTHORITY, DbContract.Repo.PATH, MATCH_ALL_REPOS);
     }
 
     private SQLiteOpenHelper sqLiteOpenHelper;
@@ -32,6 +34,25 @@ public class DbProvider extends ContentProvider {
     }
 
     @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+        int match = URI_MATCHER.match(uri);
+        long id;
+        switch (match) {
+            case MATCH_ALL_REPOS:
+                id = sqLiteOpenHelper
+                        .getWritableDatabase()
+                        .insertOrThrow(DbContract.Repo.TABLE, null, values);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid Uri: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+    @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // Implement this to handle requests to delete one or more rows.
         throw new UnsupportedOperationException("Not yet implemented");
@@ -41,12 +62,6 @@ public class DbProvider extends ContentProvider {
     public String getType(Uri uri) {
         // TODO: Implement this to handle requests for the MIME type of the data
         // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
