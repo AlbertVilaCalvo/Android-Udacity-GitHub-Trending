fd9453f90f6438e0d44837801c026d6669d176e7
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2016-09-18 16:42:58 +0200

    MainActivity refactor

--- app/src/main/java/eu/albertvila/udacity/githubtrending/MainActivity.java
+++ app/src/main/java/eu/albertvila/udacity/githubtrending/MainActivity.java
@@ -64,7 +64,83 @@ public class MainActivity extends AppCompatActivity
 
         getSupportLoaderManager().initLoader(LOADER_REPOS, null, this);
 
-        getData()
+        /*
+        getHtml()
+                .subscribeOn(Schedulers.io())
+                .map(new Function<String, List<String>>() {
+                    @Override
+                    public List<String> apply(String html) throws Exception {
+                        Document document = Jsoup.parse(html);
+                        Elements elements = document.select(".repo-list-name > a");
+                        List<String> urls = new ArrayList<>();
+                        for (int i = 0; i < elements.size(); i++) {
+                            urls.add(elements.get(i).attr("href"));
+                        }
+                        Timber.d("urls %s", urls);
+                        return urls;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DefaultObserver<List<String>>() {
+                    @Override
+                    public void onNext(List<String> value) {
+                        Timber.d("onNext: %s", value);
+                        repos.clear();
+                        repos.addAll(value);
+                        reposAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e(e, "onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Timber.i("onComplete");
+                    }
+                });
+        */
+    }
+
+    private void loadBannerAd() {
+        AdView adView = (AdView) findViewById(R.id.main_adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                // https://firebase.google.com/docs/admob/android/targeting#test_ads
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("D7A3AA3A1DAE36977EE1652523B33CBD")
+                .build();
+        adView.loadAd(adRequest);
+    }
+
+    private void setupRecyclerView() {
+        adapter = new ReposCursorAdapter();
+        recyclerView = (RecyclerView) findViewById(R.id.main_recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    // Downloads github.com/trending as String
+    private Observable<String> getHtml() {
+        final OkHttpClient client = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url("https://github.com/trending")
+                .build();
+
+        return Observable.fromCallable(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Response response = client.newCall(request).execute();
+                String html = response.body().string();
+                response.body().close();
+                return html;
+            }
+        });
+    }
+
+    // Gets the HTML, parses it and saves the data to the DB
+    private void getHtmlParseAndSaveItToDb() {
+        getHtml()
                 .subscribeOn(Schedulers.io())
                 .map(new Function<String, List<String>>() {
                     @Override
@@ -108,100 +184,6 @@ public class MainActivity extends AppCompatActivity
                         Timber.i("onComplete");
                     }
                 });
-
-        // Fetch repos
-        /*
-        getData()
-                .subscribeOn(Schedulers.io())
-                .map(new Function<String, List<String>>() {
-                    @Override
-                    public List<String> apply(String html) throws Exception {
-                        Document document = Jsoup.parse(html);
-                        Elements elements = document.select(".repo-list-name > a");
-                        List<String> urls = new ArrayList<>();
-                        for (int i = 0; i < elements.size(); i++) {
-                            urls.add(elements.get(i).attr("href"));
-                        }
-                        Timber.d("urls %s", urls);
-                        return urls;
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new DefaultObserver<List<String>>() {
-                    @Override
-                    public void onNext(List<String> value) {
-                        Timber.d("onNext: %s", value);
-                        repos.clear();
-                        repos.addAll(value);
-                        reposAdapter.notifyDataSetChanged();
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Timber.e(e, "onError");
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Timber.i("onComplete");
-                    }
-                });
-        */
-
-        /*
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url("https://github.com/trending")
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Timber.e(e, "onFailure");
-            }
-
-            @Override
-            public void onResponse(Call call, okhttp3.Response response) throws IOException {
-                Timber.d("onResponse response: %s", response);
-                // Timber.d("onResponse: %s", response.body().string());
-                Document document = Jsoup.parse(response.body().string());
-                response.body().close();
-            }
-        });
-        */
-    }
-
-    private void loadBannerAd() {
-        AdView adView = (AdView) findViewById(R.id.main_adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                // https://firebase.google.com/docs/admob/android/targeting#test_ads
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .addTestDevice("D7A3AA3A1DAE36977EE1652523B33CBD")
-                .build();
-        adView.loadAd(adRequest);
-    }
-
-    private void setupRecyclerView() {
-        adapter = new ReposCursorAdapter();
-        recyclerView = (RecyclerView) findViewById(R.id.main_recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-    }
-
-    private Observable<String> getData() {
-        final OkHttpClient client = new OkHttpClient();
-        final Request request = new Request.Builder()
-                .url("https://github.com/trending")
-                .build();
-
-        return Observable.fromCallable(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                Response response = client.newCall(request).execute();
-                String html = response.body().string();
-                response.body().close();
-                return html;
-            }
-        });
     }
 
     @Override
